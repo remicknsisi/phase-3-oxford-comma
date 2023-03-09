be4151a1d2571d029cd84b924e57c8d23f3bccde
Sienna <remicknsisi@gmail.com>
2023-03-09 12:19:33 -0500

    commit tree 1105cae7d3b4a4870ec5e948977e3c41c8758e0d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+    if array.length >= 3
+        array[array.length - 1] = "and #{array[array.length - 1]}"
+        str = array.join(", ")
+    elsif array.length == 2
+        array[2] = array[1]
+        array[1] = "and"
+        array.join(' ')
+    else
+        array.join
+    end
 
 end
